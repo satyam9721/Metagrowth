b43cdf3999b3d416ce653f3b10021cc719ac5e58
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2023-10-21 12:31:31 +0530

    Update App.js
    added routes

--- Version 1/src/App.js
+++ Version 1/src/App.js
@@ -2,11 +2,25 @@ import React from 'react'
 import Quiz from './components/Quiz'
 import QuestionComponent from "./components/QuizQuestions/QuestionComponent"
 import "./App.css"
+import { Routes, Route } from "react-router-dom"
+import QuizResult from './components/QuizResult/QuizResult'
+import Quizdisplay from './components/Quizdisplay/Quizdisplay'
 
+import Header from './components/Header/Header'
 function App() {
   return (
     <>
-      <Quiz />
+      {/* <Quizdisplay /> */}
+     {/* <Localstorage/> */}
+     {/* <QuizResult /> */}
+      <Header/>
+      <Routes>
+      <Route path="/" element={<Quizdisplay />} />
+      <Route path="/register" element={<QuizResult />} />
+     
+       
+      </Routes>
+     
     </>
   )
 }
