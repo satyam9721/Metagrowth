b2fc596a6a1a8c01edd61999376b9d42ad47d692
Satyam gupta <73402131+satyam9721@users.noreply.github.com>
2023-10-21 12:32:08 +0530

    Add files via upload
    added header

---
+++ Version 1/src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+import "./Header.css"
+const Header = () => {
+  return (
+ <div className='mb-5'>
+
+
+  <header>
+ 
+  <nav className="navbar navbar-expand-lg navbar-light" >
+    <div className="container-fluid">
+      
+      <div >
+        <ul >
+          <li>
+          <h1 className="text-center ml-6">MetaGrowth </h1>
+       
+          </li>
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
+ 
+</header >
+
+</div>
+
+  )
+}
+
+
+export default Header
